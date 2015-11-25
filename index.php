7ae006c4d741c57e8e242e6c05ec82cfc4c899a9
maurenoguera <maure.noguera@ecmanaged.com>
2015-11-25 20:02:23 +0100

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 	//require_once './DatastoreService.php'; // not autoloaded yet
 	//require_once './testModel.php';
-	require_once './Bloc.php';
+/*	require_once './Bloc.php';
 
 	$google_api_config = [
 		'application-id' => '552345224612-eias0oa0q1klu9a2q84htuo5lmrsv499.apps.googleusercontent.com',
@@ -17,7 +17,19 @@
 	print_r($posts);
 	die('k');
 
-	foo::get_visits();
+	foo::get_visits();*/
+
+
+
+	// create curl resource
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "http://metadata.google.internal/computeMetadata/v1/instance/zone");
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'X-Metadata-Flavor: Google'
+	));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$output = curl_exec($ch);
+	curl_close($ch);
 
 	$i_visits = 1;
 
