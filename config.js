72c43f10e5a822fd4b980c3918a80987dc47bcde
maurenoguera <maure.noguera@ecmanaged.com>
2015-11-25 12:56:03 +0100

    clean

--- config.js
+++
@@ -1,7 +0,0 @@
-var environment = {
-  development: { port: 8080, },
-  production: { port: 80, }
-}[process.env.NODE_ENV || 'production'];
-
-
-exports.config = environment;
--- test.watchr.rb
+++
@@ -1,7 +0,0 @@
-def run
-  system("cls")
-  system("jasmine-node ./specifications")
-  # system("jasmine-node --growl ./specifications") # optional
-end
-
-watch ('.*.js$') { run }
